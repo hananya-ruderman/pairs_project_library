a0dda7451f2756c89c80343d8dee437cf243f246
Uri Miller <usm3361@gmail.com>
2025-11-10 17:47:53 +0200

    added methods to library

--- core/library.py
+++ core/library.py
@@ -1,5 +1,6 @@
 
 from core.books import Book
+from core.user import User
 
 class Library:
     def __init__(self):
@@ -8,6 +9,23 @@ class Library:
         
     def add_book(self, book: Book):
         self.list_of_books.append(book)
+        
+    def list_available_books(self):
+        list_available_books = []
+        for book in self.list_of_books:
+            if book.is_available:
+                list_available_books.append(book)
+        return list_available_books
+    
+    def search_book(self, word):
+        for book in self.list_of_books:
+            if word in book:
+                return book
+            else:
+                return 
+                
+    def add_user(self, user: User):
+        self.list_of_users.append(user)
 
     def print_lib(self):
         for book in self.list_of_books:
@@ -15,5 +33,3 @@ class Library:
         
     def __str__(self):
         return f"the list of books is {self.list_of_books}"
-    
-
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from core.books import Book
+from core.user import User
 from core.library import Library
 
 class Main():
@@ -7,13 +8,19 @@ class Main():
 
 if __name__ == "__main__":
     library = Library()
+    # users = User()
 
     book_1 = Book("1984", "George Oroville", "1")
     book_2 = Book("A", "A author", "2")
     book_3 = Book("B", "B author", "3")
-
+    book_1.is_available = True
+    book_2.is_available = True
+    book_3.is_available = False
+    
 
     library.add_book(book_1)
     library.add_book(book_2)
     library.add_book(book_3)
     library.print_lib()
+    x = library.list_available_books()
+    print(x)
